b1fe9a6125f126b505def4c3d627b69fe6bc5a2d
Nomesque <nomesk@gmail.com>
2017-01-31 16:42:33 +1000

    Added 'touch breasts' option to scenario

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -19,20 +19,31 @@ public class TextController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			NextPage();
 		}
+		if (Input.GetKeyDown(KeyCode.T)) {
+			TouchPerson();
+		}
 	
 	}
 	
 	void NextPage() {
-		if (spacecounter == 1) {
+		spacecounter = spacecounter + 1;
+		if (spacecounter == 2) {
 			text.text = "First challenge: Seeing vs Consent";
 			}
-		else if (spacecounter == 2) {
+		else if (spacecounter == 3) {
 			text.text = "A woman standing near you takes hold of her top " +
 						"and pulls it off over her head. She's not wearing a bra. " +
-						"Her breasts are clearly visible to everyone in the room.\n " +
+						"Her breasts are clearly visible to everyone in the room.\n\n " +
 						"Press T to touch her breasts, M to talk to her, or SPACE " +
 						"to do nothing.";
 			}
-		spacecounter = spacecounter + 1;
+		
+	}
+	
+	void TouchPerson() {
+		if (spacecounter == 3) {
+			text.text = "DAHHHH DUMMMM!!!!!\n\n" +
+						"Making skin visible does not give you permission to touch it.";
+		}
 	}
 }
