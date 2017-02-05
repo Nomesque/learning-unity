77af722a8b171ca7bf63256ca47686f2a15055cb
Nomesque <nomesk@gmail.com>
2017-02-05 12:41:15 +1000

    Added first 'wrong answer' state and start of BORK game.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -6,7 +6,7 @@ public class TextController : MonoBehaviour {
 	
 	public Text text;
 	int spacecounter;
-	private enum States{Welcome, Game, Computer, Basement, Bottom_of_stairs, Landing, Top_of_stairs, Back_hallway, Loungeroom, Bathroom, Bedroom, Front_hallway, Front_yard, Footpath };
+	private enum States{Wronganswer1, Welcome, Game_start, Computer, Basement, Bottom_of_stairs, Landing, Top_of_stairs, Back_hallway, Loungeroom, Bathroom, Bedroom, Front_hallway, Front_yard, Footpath };
 	private States myState;
 	
 	// Use this for initialization
@@ -31,7 +31,15 @@ public class TextController : MonoBehaviour {
 		if (myState == States.Computer) {
 			state_computer();
 		}
-	
+		if (myState == States.Game_start) {
+			state_game_start();
+		}
+		if (myState == States.Landing) {
+			state_landing();
+		}
+		if (myState == States.Wronganswer1) {
+			state_wronganswer1();
+		}
 	}
 	
 	void state_welcome() {
@@ -80,6 +88,26 @@ public class TextController : MonoBehaviour {
 					"She starts to snore quietly.\n\n" +
 					"Press C to get her comfortable and make sure she won't choke if she throws up.\n\n" +
 					"Press T to take off the blanket and touch her.\n\n";
+					
+		// Change place to the landing if user presses C
+		if (Input.GetKeyDown(KeyCode.C)) {
+			myState = States.Landing;
+		}
+		
+		// Explain the issue with lack of consent if user presses T
+		if (Input.GetKeyDown(KeyCode.T)) {
+			myState = States.Wronganswer1;
+		}
+	}
+	void state_wronganswer1() {
+		text.text = "Sexual consent doesn't exist where a person is unconscious, asleep, or drugged. " +
+					"Touching someone who's too drunk to participate or to understand what they're " +
+					"doing isn't just 'taking advantage' - it's sexual assault.\n\n" +
+					"Press SPACE to go back to the basement.";
+		//Send the player back to the basement
+		if (Input.GetKeyDown(KeyCode.Space)){
+			myState = States.Basement;
+		}
 	}
 	
 	void state_computer() {
@@ -87,11 +115,64 @@ public class TextController : MonoBehaviour {
 					"or brand names from game makers. Huh. You look for Steam or Origin - zilch. Blizzard? " +
 					"Nothing. It's as though someone set up this computer purely for work or trolling the " +
 					"internet. Even the standard Microsoft games are absent. You shake your head disbelievingly. " +
-					"An icon on the desktop catches your eye: a Z filled in with bricks, sort of like the old " +
+					"An icon on the desktop catches your eye: a B filled in with bricks, sort of like the old " +
 					"Zork logo, but subtly different. Could it be a Zork clone game? Well, text games are " +
 					"pretty dorky, but any port in a storm, right?\n\n" +
 					"Press P to double-click on the Z icon.\n\n" +
 					"Press L to leave the computer and go outside.";
+		// Change place to the bottom of the stairs if user presses L
+		if (Input.GetKeyDown(KeyCode.P)) {
+			myState = States.Game_start;
+		}
+		// Change place to the computer if user presses C
+		if (Input.GetKeyDown(KeyCode.L)) {
+			myState = States.Basement;
+		}
 	}
 	
+	void state_game_start() {
+		text.text = "The screen to your right goes blank, then displays a large black window with " +
+					"an ascii outline of one large word: " +
+					" =====\\    /====\\    =====\\    | |    // \n" +
+  					"| |    \\  //     \\   | |   \\   | |   // \n" +
+  					"| |    // ||       ||  | |   ||   | |  // \n" +
+  					"| |===<<  ||       ||  | |==<<    | |<< \n" +
+  					"| |    \\ ||       ||  | |   \\   | |  \\ \n" +
+  					"| |    //  \\     //   | |    \\  | |   \\ \n" +
+  					" =====//    \\===//    | |     || | |    \\ \n";
+  	}
+  					
+  					
+	void state_landing() {
+		text.text = "Hey, brave move! But you have a challenge to face before you can get out.\n\n" +
+				"A flash of light dazzles you, then fades away. When your eyes clear, you " +
+				"can see a large, dark form on your couch. It might be... someone lying there? " +
+				"You creep closer to figure out what it is, and as you do so, you move from between " +
+				"the shape and a monitor, letting light flood over it. It groans and moves. It's " +
+				"human! And... female? The person starts to sit up, then flops back into a prone " +
+				"position.\n\n" +
+				"'I'm so drunk,' she says. 'I can't even sit up. Wow, I've never been this drunk.' " +
+				"She starts to snore quietly.\n\n" +
+				"Press C to get her comfortable and make sure she won't choke if she throws up.\n\n" +
+				"Press T to take off the blanket and touch her.\n\n";
+		
+		// Change place to the landing if user presses C
+		if (Input.GetKeyDown(KeyCode.C)) {
+			myState = States.Landing;
+		}
+		
+		// Change place to the computer if user presses T
+		if (Input.GetKeyDown(KeyCode.T)) {
+			text.text = "Sexual consent doesn't exist where a person is unconscious, asleep, or drugged. " +
+				"Touching someone who's too drunk to participate or to understand what they're " +
+					"doing isn't just 'taking advantage' - it's sexual assault.\n\n" +
+					"Press SPACE to go back to the basement.";
+			if (Input.GetKeyDown(KeyCode.Space)){
+				myState = States.Basement;
+			}
+		}
+	}		
+  					
+  					
+  					
 }
